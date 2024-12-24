15e8de50e6b8b38987161b5b8637583340edd9a7
AfaqShahidKhan <afaqk3203@gmail.com>
2024-12-24 12:57:42 +0500

    modify error handling

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -21,6 +21,14 @@ exports.getAllTasks = async (req, res) => {
 exports.getTask = async (req, res) => {
   try {
     const task = await Task.findById(req.params.id);
+    console.log(`Task is ${task}`);
+    
+    if(!task){
+        return res.status(404).json({
+            status: "failed",
+            message: "No task found with that ID",
+        });
+    }
     res.status(200).json({
       status: "success",
       data: {
@@ -58,6 +66,12 @@ exports.updateTask = async (req, res) => {
       new: true,
       runValidators: true,
     });
+    if(!task){
+        return res.status(404).json({
+            status: "failed",
+            message: "No task found with that ID",
+        });
+    }
     res.status(200).json({
       status: "success",
       data: {
@@ -75,6 +89,12 @@ exports.updateTask = async (req, res) => {
 exports.deleteTask = async (req, res) => {
   try {
     const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task){
+        return res.status(404).json({
+            status: "failed",
+            message: "No task found with that ID",
+        });
+    }
     console.log(`Deleted task is ${task}`);
 
     res.status(204).json({
