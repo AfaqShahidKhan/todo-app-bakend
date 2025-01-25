1932b4944bb508fd502516dffacabc2b1e2cd7a8
AfaqShahidKhan <afaqk3203@gmail.com>
2025-01-25 12:32:49 +0500

    modify path

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(xss());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: 'http://localhost:5000',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: 'Content-Type,Authorization',
 };
